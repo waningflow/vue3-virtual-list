2ee0ddb0e348314b5f2c8a361c3b7f40320c9eb5
waningflow <jack1995woods@gmail.com>
2020-12-05 11:12:17 +0800

    feat: init project

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <VueVirtualList msg="Welcome to Your Vue.js + TypeScript App" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import VueVirtualList from "./vue3-virtual-list.vue";
 
 export default defineComponent({
   name: "App",
   components: {
-    HelloWorld
+    VueVirtualList
   }
 });
 </script>
---
+++ src/vue3-virtual-list.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="vue3-virtual-table-container">
+    vvv
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "VueVirtualList",
+  props: {
+    msg: String
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+.vue3-virtual-table-container {
+  width: 500px;
+  height: 200px;
+}
+</style>
